fb7e2059357ca87eabfaae74de00c3ac1ac84443
gateleaker <chendavid1919@gmail.com>
2026-07-02 15:49:55 +1000

    actions function finish

--- Week 0 - Search/tictactoe/tictactoe.py
+++ Week 0 - Search/tictactoe/tictactoe.py
@@ -4,6 +4,8 @@ Tic Tac Toe Player
 
 import math
 
+from pygame import colordict
+
 X = "X"
 O = "O"
 EMPTY = None
@@ -22,21 +24,40 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-
+    xcount = sum(instance.count(X) for instance in board)
+    if xcount % 2 == 0:
+        return X
+    else:
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    # actions = []
+    # while actions != []:
+    #     for i, j in board[i][j]:
+    #         if j == EMPTY:
+    #             actions.append((i,j))
+    #         else:
+    #             return actions
+    # return None
+
+    actions = set()
+    for i, row in enumerate(board):
+        for j, column in enumerate(row):  # refer to diagram
+            if row[j] == EMPTY:
+                actions.add((i, j))
+    return actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    # check row, column, one diagonal, one antidiagonal
+
 
 
 def winner(board):
